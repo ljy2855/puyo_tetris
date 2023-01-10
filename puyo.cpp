7fa38dc484beb0b8edaee1d986e3e56031b243df
ljy2855 <ljy2855@naver.com>
2023-01-10 15:02:21 +0900

    merge

--- puyo.cpp
+++ puyo.cpp
@@ -697,33 +697,6 @@ int hostname_to_ip(char * hostname , char* ip)
 	return 0;
 }
 
-int hostname_to_ip(char * hostname , char* ip)
-{
-	int sockfd;  
-	struct addrinfo hints, *servinfo, *p;
-	struct sockaddr_in *h;
-	int rv;
-
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC; // use AF_INET6 to force IPv6
-	hints.ai_socktype = SOCK_STREAM;
-
-	if ( (rv = getaddrinfo( hostname , "http" , &hints , &servinfo)) != 0) 
-	{
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
-	}
-
-	// loop through all the results and connect to the first we can
-	for(p = servinfo; p != NULL; p = p->ai_next) 
-	{
-		h = (struct sockaddr_in *) p->ai_addr;
-		strcpy(ip , inet_ntoa( h->sin_addr ) );
-	}
-	
-	freeaddrinfo(servinfo); // all done with this structure
-	return 0;
-}
 
 void * recv_data(void * arg){
 	int socket = *((int*)arg);
